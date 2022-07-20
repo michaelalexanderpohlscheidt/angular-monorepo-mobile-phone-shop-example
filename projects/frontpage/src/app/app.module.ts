bb9ea35b6fe1aa4bf3b70db227b8df88eae38fba
Michael Alexander Pohlscheidt <83654391+michaelalexanderpohlscheidt@users.noreply.github.com>
2022-07-20 20:24:58 +0200

    add navbar component and div wrapper element

--- projects/frontpage/src/app/app.module.ts
+++ projects/frontpage/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
